b554a13e4b14ee43f9e4c8a4f00be9b7104e200e
Evan Mackay <evanmackay@Evans-MacBook-Pro.local>
2020-09-29 19:35:01 -0500

    added framwork for employee and engineer

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const Employee = require("./lib/Employee")
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+console.log('hello')
+let emp = new Employee("Tod", 1234, "tod@email.com")
+console.log(emp)
+let eng = new Engineer("Tod", 1234, "tod@email.com", "todsgithub")
+console.log(eng)
 
 
 // Write code to use inquirer to gather information about the development team members,
