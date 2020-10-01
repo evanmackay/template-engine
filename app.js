5684973c3735bc69aec1ef8a3a62f69f085802ca
Evan Mackay <evanmackay@Evans-MacBook-Pro.local>
2020-10-01 16:18:22 -0500

    fixed a console log and add comments to clarify code.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// files to require
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -9,14 +10,15 @@ const fs = require("fs");
 
 
 
-
+// directories and paths
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
+// render function to create html
 const render = require("./lib/htmlRenderer");
 
-
+// employees array to be used in render function
 const employees = []
+// function to ask initial questions to the user
 function initialQuestions() {
     inquirer
     .prompt([
@@ -27,6 +29,7 @@ function initialQuestions() {
             name: "initial"
         }
     ])
+    // asks specific questions based on the users pick
     .then(function(response){
         if (response.initial === 'Manager'){
             managerQuestions()
@@ -36,7 +39,11 @@ function initialQuestions() {
             internQuestions()
         }
     })
+    .catch((err) => {
+        console.log(err)
+    })
 }
+// function ran after user has answered questions. allows the user to conitinue to build there team or to publish their work.
 function continuedQuestions() {
     inquirer
         .prompt([
@@ -55,11 +62,16 @@ function continuedQuestions() {
             } else if (response.otherinitial === 'Intern') {
                 internQuestions()
             } else {
+                // writes the users team file
                 console.log('Your team has been built!')
                 fs.writeFileSync(outputPath, render(employees), 'utf-8')
             }
         })
+        .catch((err) => {
+            console.log(err)
+        })
 }
+// manager specific questions
 function managerQuestions() {
     inquirer
         .prompt([
@@ -94,6 +106,7 @@ function managerQuestions() {
         })
     
 }
+// engineer specific questions
 function engineerQuestions() {
     inquirer
         .prompt([
@@ -123,7 +136,11 @@ function engineerQuestions() {
             employees.push(engineer)
             continuedQuestions()
         })
+        .catch((err) => {
+            console.log(err)
+        })
 }
+// intern specific questions
 function internQuestions() {
     inquirer
         .prompt([
@@ -157,31 +174,5 @@ function internQuestions() {
             console.log(err)
         })
 }
+// runs the initial questions function
 initialQuestions()
-
-    
-    
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
