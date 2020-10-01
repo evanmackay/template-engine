94d6bd0f0fb760e67f846417c029e2ab69514174
Evan Mackay <evanmackay@Evans-MacBook-Pro.local>
2020-10-01 16:03:49 -0500

    fixed file structure

--- app.js
+++ app.js
@@ -4,17 +4,163 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const Employee = require("./lib/Employee")
+
+
+
+
+
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-console.log('hello')
-let emp = new Employee("Tod", 1234, "tod@email.com")
-console.log(emp)
-let eng = new Engineer("Tod", 1234, "tod@email.com", "todsgithub")
-console.log(eng)
+
+
+const employees = []
+function initialQuestions() {
+    inquirer
+    .prompt([
+        {
+            type: "list",
+            message: "Welcome to our Team Building tool. Please select a role Below.",
+            choices: ['Manager', 'Engineer', 'Intern'],
+            name: "initial"
+        }
+    ])
+    .then(function(response){
+        if (response.initial === 'Manager'){
+            managerQuestions()
+        } else if (response.initial === 'Engineer') {
+            engineerQuestions()
+        } else {
+            internQuestions()
+        }
+    })
+}
+function continuedQuestions() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Select a Role below or select Done if you are finished.",
+                choices: ['Manager', 'Engineer', 'Intern', 'Done'],
+                name: "otherinitial"
+            }
+        ])
+        .then(function(response) {
+            if (response.otherinitial === 'Manager') {
+                managerQuestions()
+            } else if (response.otherinitial === 'Engineer') {
+                engineerQuestions()
+            } else if (response.otherinitial === 'Intern') {
+                internQuestions()
+            } else {
+                console.log(employees)
+                render(employees)
+            }
+        })
+}
+function managerQuestions() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is your Manager's name?",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "What is your Manager's ID number?",
+                name: "idnumber"
+            },
+            {
+                type: "input",
+                message: "What is your Manager's email address?",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "What is your Manager's office number?",
+                name: "officenumber"
+            }
+        ])
+        .then(function(response){
+            let manager = new Manager(response.name, response.idnumber, response.email, response.officenumber)
+            employees.push(manager)
+            continuedQuestions()
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    
+}
+function engineerQuestions() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is your Engineer's name?",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "What is your Engineer's ID number?",
+                name: "idnumber"
+            },
+            {
+                type: "input",
+                message: "What is your Engineer's email address?",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "What is your Engineer's GitHub username?",
+                name: "githubuser"
+            }
+        ])
+        .then(function(response) {
+            let engineer = new Engineer(response.name, response.idnumber, response.email, response.githubuser)
+            employees.push(engineer)
+            continuedQuestions()
+        })
+}
+function internQuestions() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is your Intern's name?",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "What is your Intern's ID number?",
+                name: "idnumber"
+            },
+            {
+                type: "input",
+                message: "What is your Intern's email address?",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "What school did your Intern attend?",
+                name: "school"
+            }
+        ])
+        .then(function(response){
+            let intern = new Intern(response.name, response.idnumber, response.email, response.school)
+            employees.push(intern)
+            continuedQuestions()
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
+initialQuestions()
+
+    
+    
 
 
 // Write code to use inquirer to gather information about the development team members,
